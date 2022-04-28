9e5952e6bb9580ae2d436b9d2822e60786d75378
Favour Max-Oti <maxotif@gmail.com>
2022-04-28 22:54:55 +0100

    finished routes file

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,1 +1,18 @@
 package main
+
+import(
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/maximof/go-bookstore/pkg/routes"
+	"fmt"
+)
+
+func main(){
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	fmt.Printf("Starting server at port 8002\n")
+	log.Fatal(http.ListenAndServe("localhost:8002", r))
+}
